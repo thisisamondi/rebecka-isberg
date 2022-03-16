5fc39e6bb787230e120ae6687f9be14940d09c8d
thisisamondi <67878188+thisisamondi@users.noreply.github.com>
2022-03-16 14:36:03 +0100

    add animation to title in home

--- components/Home.tsx
+++ components/Home.tsx
@@ -2,7 +2,7 @@ const HomePage = ({ title, image }: any) => {
   return (
     <div className="flex min-h-screen bg-black py-6 px-6 text-white sm:relative md:py-10">
       <div className="ml-52 mt-40 w-6/12">
-        <h1 className=" text-6xl text-white">{title}</h1>
+        <h1 className="animate-fade-in-down text-6xl text-white">{title}</h1>
       </div>
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,23 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'fade-in-down': {
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(-40px)',
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateY(0)',
+          },
+        },
+      },
+      animation: {
+        'fade-in-down': 'fade-in-down 1s ease-out',
+      },
+    },
   },
   plugins: [],
 }
