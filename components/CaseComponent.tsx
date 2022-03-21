faba035e0276e548d38be6dde88e8ce1db3a4816
thisisamondi <67878188+thisisamondi@users.noreply.github.com>
2022-03-21 16:19:36 +0100

    wrap all page components in container component

--- components/CaseComponent.tsx
+++ components/CaseComponent.tsx
@@ -1,38 +1,43 @@
 import Image from 'next/image'
+import Container from './Container'
 
 const CaseComponent = ({ title, description, artist, category }: any) => {
   return (
-    <div className="min-h-screen bg-black text-white">
-      {/* TITLE */}
-      <h1 className="text-center text-7xl">{title}</h1>
-      {/* DESCRIPTION */}
-      <p className="mb-10 text-center">{description}</p>
-      {/* GRID CONTAINER */}
-      <div className="flex justify-center">
-        {/* GRID */}
-        <div className="grid gap-10 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-          {artist.map((artist: any, index: any) => (
-            <div key={index}>
-              <div className="relative mb-5 sm:h-[15rem] sm:w-[15rem] md:h-[15rem] md:w-[15rem] lg:h-[23rem] lg:w-[23rem]">
-                <Image
-                  src={artist.image}
-                  layout="fill"
-                  objectFit="cover"
-                  alt={artist.image.alt}
-                />
+    <Container>
+      <div className="min-h-screen bg-black text-white">
+        {/* TITLE */}
+        <h1 className="text-center text-7xl">{title}</h1>
+        {/* DESCRIPTION */}
+        <p className="mb-10 text-center">{description}</p>
+        {/* GRID CONTAINER */}
+        <div className="flex justify-center">
+          {/* GRID */}
+          <div className="grid gap-10 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+            {artist.map((artist: any, index: any) => (
+              <div key={index}>
+                <div className="relative mb-5 sm:h-[15rem] sm:w-[15rem] md:h-[15rem] md:w-[15rem] lg:h-[23rem] lg:w-[23rem]">
+                  <Image
+                    src={artist.image}
+                    layout="fill"
+                    objectFit="cover"
+                    alt={artist.image.alt}
+                  />
+                </div>
+                <p className="mt-1">{artist.name}</p>
+                {/* CATEGORY */}
+                <p>{category[0].title}</p>
+                {/* COUNTRY */}
+                {artist.country.map((country: any, index: any) => (
+                  <span key={index}>
+                    {(index ? ', ' : '') + country.country}
+                  </span>
+                ))}
               </div>
-              <p className="mt-1">{artist.name}</p>
-              {/* CATEGORY */}
-              <p>{category[0].title}</p>
-              {/* COUNTRY */}
-              {artist.country.map((country: any, index: any) => (
-                <span key={index}>{(index ? ', ' : '') + country.country}</span>
-              ))}
-            </div>
-          ))}{' '}
+            ))}{' '}
+          </div>
         </div>
       </div>
-    </div>
+    </Container>
   )
 }
 
--- components/Home.tsx
+++ components/Home.tsx
@@ -1,10 +1,13 @@
+import Container from './Container'
 const HomePage = ({ title, image }: any) => {
   return (
-    <div className="flex min-h-screen bg-black py-6 px-6 text-white sm:relative md:py-10">
-      <div className="ml-52 mt-40 w-6/12">
-        <h1 className="animate-fade-in-down text-6xl text-white">{title}</h1>
+    <Container>
+      <div className="min-h-screen bg-black text-white sm:relative md:py-10">
+        <div className="mt-40 w-6/12">
+          <h1 className="animate-fade-in-down text-6xl text-white">{title}</h1>
+        </div>
       </div>
-    </div>
+    </Container>
   )
 }
 
