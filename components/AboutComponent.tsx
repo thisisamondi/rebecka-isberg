e839ade572e8db29b4c4244ec1c6a5ad3dc79e70
thisisamondi <67878188+thisisamondi@users.noreply.github.com>
2022-03-23 21:59:39 +0100

    add title to about component

--- components/AboutComponent.tsx
+++ components/AboutComponent.tsx
@@ -1,13 +1,15 @@
 import Container from './Container'
 import { NextImage } from './images/NextFigure'
 
-const InfoComponent = ({ description, image }: any) => {
+const InfoComponent = ({ description, image, title }: any) => {
   return (
     <Container>
+      {' '}
       <div className="flex min-h-screen justify-center">
         <div className="w-4/5 bg-black text-white">
+          <h1 className="mb-10 text-center text-white lg:text-6xl">{title}</h1>
           <NextImage image={image} />
-          <div className="max-w-[500px]">
+          <div className="mt-5 max-w-[500px]">
             <p>{description}</p>
           </div>
         </div>
